02086f7f20bc6cd7e6969f42b53221181ec73cf0
sarah-ga <sarah.hallacher@generalassemb.ly>
2016-08-12 16:05:16 -0400

    unit-6-lesson-4

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('button').on('click', function () {
+
+	// Add your code here!
+
+	// Step 1: Add the party class to the .square
+
+
+	// Step 2: Use the .attr() method to update the src of the image to 'images/cat.jpeg'
+
+
+	// Step 3: Change the HTML for the h1 to "Having fun!" using the .html() method.
+
+
+	// Step 4: Prepend the following paragraph to the body element:
+	//    '<p>Done and Done!</p>'
+
+
+});
